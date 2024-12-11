fa0898dd03413dc9f6e0cadb5b85c082dea15038
DoriaFabio <doriafabio2001@gmail.com>
2024-12-11 19:53:44 +0100

    add tasks.js and add file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import tasks from "./data/tasks.js";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  //parte logica
+  
+  //parte HTML
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="container">
+        <h2>Pizze con mozzarella</h2>
+        <ul></ul>
+        <h2>Pizze senza mozzarella</h2>
+        <ul></ul>
+      </main>
+      <footer>sono il footer</footer>
     </>
   )
 }
 
-export default App
+export default App;
