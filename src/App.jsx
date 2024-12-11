53ecad50ab8fe0eb12d32e168449c7e7ea3e5235
DoriaFabio <doriafabio2001@gmail.com>
2024-12-11 20:55:13 +0100

    do list and do navbar with css

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,51 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import tasks from "./data/tasks.js";
+import HeaderComponents from "./components/Headercomponents.jsx";
 import './App.css'
 
 function App() {
   //parte logica
-  
+  const arrayIncomplete = tasks.filter((element) => {
+    return element.state.includes("in_progress") || element.state.includes("backlog")
+  })
+  const arrayComplete = tasks.filter((element) => {
+    return element.state.includes("completed")
+  })
+
+  const incLength = arrayIncomplete.length;
+  const compLength = arrayComplete.length;
+
+  const incomplete = arrayIncomplete.map((element) => {
+    return <li key={element.id} className="list-unstyled">
+      <h6>{element.title} <span className="badge text-bg-warning p-2">{element.state}</span></h6>
+      <p>{`Priorità: ${element.priority}`}</p>
+      <p>{`Tempo stimato: ${element.estimatedTime}`}</p>
+    </li>
+  })
+
+  const complete = arrayComplete.map((element) => {
+    return <li key={element.id} className="list-unstyled">
+      <h6>{element.title} <span className="badge text-bg-success p-2">{element.state}</span></h6>
+      <p>{`Priorità: ${element.priority}`}</p>
+      <p>{`Tempo stimato: ${element.estimatedTime}`}</p>
+    </li>
+  })
   //parte HTML
   return (
     <>
-      <main className="container">
-        <h2>Pizze con mozzarella</h2>
-        <ul></ul>
-        <h2>Pizze senza mozzarella</h2>
-        <ul></ul>
+      <HeaderComponents />
+      <main>
+        <h5>Current Tasks <span>{`(${incLength})`}</span></h5>
+        <ul>
+          {incomplete}
+        </ul>
+        <hr />
+        <h5>Completed Tasks <span>{`(${compLength})`}</span></h5>
+        <ul>
+          {complete}
+        </ul>
       </main>
-      <footer>sono il footer</footer>
     </>
   )
 }
---
+++ src/components/Headercomponents.jsx
@@ -0,0 +1,11 @@
+function HeaderComponent() {
+    const title = "Task Manager";
+  
+    return (
+      <header>
+        <h4 className="mybg p-3 ">{title}</h4>
+      </header>
+    );
+  }
+  
+  export default HeaderComponent;
